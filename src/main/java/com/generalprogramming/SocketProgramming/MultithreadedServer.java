bb42c5adf58f33e17972b3754cfde5ffc096232b
Devdatta Kanawade <Kanawade_D@wyde.eagan.local>
2018-05-03 13:49:12 -0400

    fixed the issue of inifite processing of server thread

--- src/main/java/com/generalprogramming/SocketProgramming/MultithreadedServer.java
+++ src/main/java/com/generalprogramming/SocketProgramming/MultithreadedServer.java
@@ -9,12 +9,16 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MultithreadedServer {
-    private static BlockingQueue<Socket> queuedSockets = new ArrayBlockingQueue<>(1);                  //max queued connections.
-    private static Semaphore semaphoreForMaxConnectionsAllowed = new Semaphore(2);              //max active connections being served.
+    private static final int MAX_QUEUE_SIZE = 1;
+    private static BlockingQueue<Socket> queuedSockets = new ArrayBlockingQueue<>(MAX_QUEUE_SIZE);                  //max queued connections.
+    private static final int MAX_THREAD_RUNNING = 2;
+    private static Semaphore semaphoreForMaxConnectionsAllowed = new Semaphore(MAX_THREAD_RUNNING);              //max active connections being served.
 
-    private static void handleClientConnectionRequest(final Socket newSocketForClientConnection, final Semaphore maxConnectionSemaphore) {
+    private static void handleClientConnectionRequest(final Socket newSocketForClientConnection, final Semaphore maxConnectionSemaphore,
+                                                      final Object mainThreadListeningLock) {
         new Thread(new Runnable() {
 
             @Override
@@ -30,15 +34,14 @@ public class MultithreadedServer {
                     String serverMsg;
                     String clientMsg;
 
-                    SocketAddress clientSocket = (InetSocketAddress) newSocketForClientConnection.getRemoteSocketAddress();
-
                     while ((clientMsg = socketReader.readLine()) != null) {
                         if (clientMsg.equalsIgnoreCase("quit")) {
                             maxConnectionSemaphore.release();
+                            synchronized (mainThreadListeningLock){
+                                mainThreadListeningLock.notifyAll();
+                            }
                             break;
                         }
-
-                        System.out.println("client with socket " + clientSocket + " sent MSG : " + clientMsg);
                         serverMsg = reverseString(clientMsg);
 
                         socketWriter.println(serverMsg);
@@ -80,30 +83,39 @@ public class MultithreadedServer {
         try {
             Integer portNumber = Integer.parseInt(args[0]);
             serverSocket = new ServerSocket(portNumber);
-            int connectionNumber = 0;
+            Object mainThreadListeningLock = new Object();
 
             System.out.println("Server listening on port# : " + args[0]);
 
             //main thread...
             while (shouldContinue) {
                 Socket newServerSocketForClientConnection = null;
+
+                //if queue is not full yet, then we can accept the connection request. Worst case (if not monitors available through
+                //semaphore), it will be queued.
                 newServerSocketForClientConnection = queuedSockets.poll();
 
                 if (newServerSocketForClientConnection == null) {
                     newServerSocketForClientConnection = serverSocket.accept();
 
-                    connectionNumber++;
-                    System.out.println("Created new socket upon client request. ConnectionCOunt = " + connectionNumber);
-
-                    processConnection(newServerSocketForClientConnection);
+                    processConnection(newServerSocketForClientConnection, mainThreadListeningLock);
                 } else {
                     //i.e. queue has a socket request pending.
-                    System.out.println("Picking queued socket..");
-                    processConnection(newServerSocketForClientConnection);
+                    synchronized (mainThreadListeningLock) {
+                        System.out.println("Semaphore Permits available .." + semaphoreForMaxConnectionsAllowed.availablePermits());
+                        while (semaphoreForMaxConnectionsAllowed.availablePermits() == 0) {
+                            //i.e. there exists a connectionRequest in queue, but there are no resources available to respect the request
+                            System.out.println("Server thread is not accepting any more connections until resources to respect the connections are free.");
+                            mainThreadListeningLock.wait();
+                        }
+                        processConnection(newServerSocketForClientConnection, mainThreadListeningLock);
+                    }
+
                 }
             }
-
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
             if (serverSocket != null) {
@@ -112,18 +124,17 @@ public class MultithreadedServer {
         }
     }
 
-    private static void processConnection(Socket newServerSocketForClientConnection) {
+    private static void processConnection(Socket newServerSocketForClientConnection, Object processConnection) {
 
         if (semaphoreForMaxConnectionsAllowed.availablePermits() > 0) {
-            handleClientConnectionRequest(newServerSocketForClientConnection, semaphoreForMaxConnectionsAllowed);
+            handleClientConnectionRequest(newServerSocketForClientConnection, semaphoreForMaxConnectionsAllowed, processConnection);
         } else {
-            //System.out.println("Since exceeded max connection limit, adding in queue.");
+
             if (queuedSockets.offer(newServerSocketForClientConnection)) {
                 System.out.println("connectionRequest queued because no more space on server. QueuedSocketList size : " + queuedSockets.size());
-            }else{
-                System.out.println("No space available for client connections. Can not be queued too.");
+            } else {
+                System.out.println("No space available for client connections and can not be queued too. Please try again later.");
             }
-
         }
 
     }
